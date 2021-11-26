3bb3a42546887b6609fe0adc5dad07a4f347d9cd
sunlin <sunlin@kingsoft.com>
2021-11-26 14:10:50 +0800

    new

--- src/main/java/com/starfish/jsonformat/ui/JsonFormatToolWindow.java
+++ src/main/java/com/starfish/jsonformat/ui/JsonFormatToolWindow.java
@@ -1,9 +1,15 @@
 package com.starfish.jsonformat.ui;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
+import lombok.extern.slf4j.Slf4j;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+
+@Slf4j
 public class JsonFormatToolWindow {
 
     private static JsonFormatForm jsonFormatForm;
@@ -11,19 +17,29 @@ public class JsonFormatToolWindow {
     public static JComponent getComponent() {
         jsonFormatForm = new JsonFormatForm();
 
-        addListener(jsonFormatForm);
+        addListener();
 
         return jsonFormatForm.getMainPanel();
     }
 
-    public static void addListener(JsonFormatForm jsonFormatForm) {
+    public static void addListener() {
         // 给复制按钮加上监听器
-        JButton copyJsonButton =  jsonFormatForm.getCopyJsonButton();
+        JButton copyJsonButton = jsonFormatForm.getCopyJsonButton();
         copyJsonButton.addActionListener(e -> {
-            JTextArea jsonTextArea = jsonFormatForm.getJsonTextArea();
-            String text = jsonTextArea.getText();
-            System.out.println(text);
+            onCopyJson();
         });
     }
+
+    public static void onCopyJson() {
+        JTextArea jsonTextArea = jsonFormatForm.getJsonTextArea();
+        String text = jsonTextArea.getText();
+        log.info("json={}",text);
+
+        //  文本设置到剪贴板中
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        Transferable transferable = new StringSelection(text);
+        clipboard.setContents(transferable, null);
+    }
+
 
 }
